91f41b35d77e4bc8e98ddd58923b85f94f7470ee
david <david@localhost.localdomain>
2019-04-12 23:26:15 -0500

    Start on addition of block matrices

--- src/BMElements.jl
+++ src/BMElements.jl
@@ -1,4 +1,5 @@
 import Base.getindex
+import Base.+
 
 function getindex(A::BlockMatrix{T}, i::Int64, j::Int64) where {T}
     row::Int64 = ceil(i/A.block_dims[1])
@@ -6,10 +7,16 @@ function getindex(A::BlockMatrix{T}, i::Int64, j::Int64) where {T}
     ii::Int64 = (i-1)%A.block_dims[1] + 1
     jj::Int64 = (j-1)%A.block_dims[2] + 1
 
-    println("$row, $col, $ii, $jj")
     return A.matrix[row,col][ii,jj]
 end
 
 function getblock(A::BlockMatrix{T}, i::Int64, j::Int64) where {T}
     return A.matrix[i,j]
+end
+
+function +(x::BlockMatrix{Number}, y::BlockMatrix{Number})
+    for i in 1:x.mdims_blockwise[1], j in x.mdims_blockwise[2]
+        x[i,j] += y[i,j]
+    end
+    return x
 end
